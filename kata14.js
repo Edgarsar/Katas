e3c2f505f49a7e8eda55aceb3c4d87a9fe8a568d
Edgarsar <edgarsargsyan@hotmail.com>
2022-09-23 02:09:01 +0000

    Implement a solution for the calculateChange function

--- kata14.js
+++ kata14.js
@@ -2,21 +2,77 @@
 // Return a new object that describes the total amount of change for the cashier to give back. 
 // Omit any types of change that you shouldn't give back, i.e. if you don't give back a twenty dollar bill,
 // don't include it in the results.
-
+//  $10 will be represented as 1000.
 // Valid denominations are as follows:
+// Twenty dollars === 2000
+// Ten dollars === 1000
+// Five dollars === 500
+// Two dollars === 200
+// One dollar === 1000
+// Quarter (25¢) === 25
+// Dime (10¢) === 10
+// Nickel (5¢) === 5
+// Penny (1¢) ==== 1
 
-// Twenty dollars
-// Ten dollars
-// Five dollars
-// Two dollars
-// One dollar
-// Quarter (25¢)
-// Dime (10¢)
-// Nickel (5¢)
-// Penny (1¢)
+const calculateChange = function (total, cash) {
+  let changeObj = {
+    twentyDollar: 0,
+    tenDollar: 0,
+    fiveDollar: 0,
+    tenDollar: 0,
+    twoDollar: 0,
+    oneDollar: 0,
+    quarter: 0,
+    dime: 0,
+    nickel: 0,
+    penny: 0
+  };
 
-const calculateChange = function(total, cash) {
-  // Your code here
+  let change = cash - total;
+  for (let i = 0; i <= change; i++) {
+
+    if (change >= 2000) {
+      changeObj["twentyDollar"] += 1;
+      change = change - 2000;
+    }
+    else if (change >= 1000) {
+      changeObj["tenDollar"] += 1;
+      change = change - 1000;
+    }
+    else if (change >= 500) {
+      changeObj["fiveDollar"] += 1;
+      change = change - 500;
+    }
+    else if (change >= 200) {
+      changeObj["twoDollar"] += 1;
+      change = change - 200;
+    }
+    else if (change >= 100) {
+      changeObj["oneDollar"] += 1;
+      change = change - 100;
+    }
+    else if (change >= 25) {
+      changeObj["quarter"] += 1;
+      change = change - 25;
+    }
+    else if (change >= 10) {
+      changeObj["dime"] += 1;
+      change = change - 10;
+    }
+    else if (change >= 5) {
+      changeObj["nickel"] += 1;
+      change = change - 5;
+    }
+
+    changeObj["penny"] = change;;
+
+  }
+  for (const key in changeObj){
+    if (changeObj[key] === 0){
+      delete changeObj[key];
+    }
+  }
+  return changeObj
 };
 
 console.log(calculateChange(1787, 2000));
@@ -24,7 +80,4 @@ console.log(calculateChange(2623, 4000));
 console.log(calculateChange(501, 1000));
 
 
-// { twoDollar: 1, dime: 1, penny: 3 }
-// { tenDollar: 1, twoDollar: 1, oneDollar: 1, quarter: 3, penny: 2 }
-// { twoDollar: 2, quarter: 3, dime: 2, penny: 4 }
-// 
+
