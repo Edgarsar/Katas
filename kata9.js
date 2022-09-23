6d964bb765309254abf1a4b065ff46a735c5742b
Edgarsar <edgarsargsyan@hotmail.com>
2022-09-23 02:21:10 +0000

    Implement a solution for the camelCase function

--- kata9.js
+++ kata9.js
@@ -1,1 +1,18 @@
 // Create a function named camelCase that will convert a string to camel case, and return the result.
+
+const camelCase = function (input) {
+  const splitInput = input.split(" ");
+
+  if (splitInput.length <= 1) {
+    return input;
+  }
+
+  return splitInput.map((e) => {
+    return e.charAt(0).toUpperCase() + e.substring(1, e.length).toLowerCase();
+  }).join("");
+};
+
+console.log(camelCase("this is a string"));
+console.log(camelCase("loopy lighthouse"));
+console.log(camelCase("supercalifragalisticexpialidocious"));
+
