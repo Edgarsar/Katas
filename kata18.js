ede1681bae221599029f12611fa38097a400ddc9
Edgarsar <edgarsargsyan@hotmail.com>
2022-09-27 21:55:10 +0000

    Implement solution for the squareCode function

--- kata18.js
+++ kata18.js
@@ -1,8 +1,22 @@
 //Create a function named squareCode that will receive a message, and return the secret square code version of the message.
 
 
-const squareCode = function(message) {
-  // Put your solution here
+const squareCode = function (message) {
+  let noSpaces = message.replace(/\s/g, '');
+  const math = Math.ceil(Math.sqrt(noSpaces.length));
+
+  let result = '';
+
+  for (let i = 0; i < math; i++) {
+
+    for (let j = i; j < noSpaces.length; j += math) {
+
+      result += noSpaces[j];
+    }
+    result += ' ';
+  }
+
+  return result;
 };
 
 console.log(squareCode("chill out"));
