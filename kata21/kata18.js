ee87c5df7a507c9c6b07a8f869ce7720e3ec64ec
Edgarsar <edgarsargsyan@hotmail.com>
2022-09-27 21:34:06 +0000

    Create a new file kata18 and write function description

---
+++ kata21/kata18.js
@@ -0,0 +1,18 @@
+//Create a function named squareCode that will receive a message, and return the secret square code version of the message.
+
+
+const squareCode = function(message) {
+  // Put your solution here
+};
+
+console.log(squareCode("chill out"));
+console.log(squareCode("feed the dog"));
+console.log(squareCode("have a nice day"));
+console.log(squareCode("if man was meant to stay on the ground god would have given us roots"));
+
+// output
+
+// clu hlt io  
+// fto ehg ee dd
+// hae and via ecy
+// imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn sseoau
