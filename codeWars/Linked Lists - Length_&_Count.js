70f35d8d7b0f5b45ff3bb8a4d1e30e5839203dc6
Edgar <edgarsargsyan@hotmail.com>
2023-02-16 14:10:20 -0600

    Solution kata 6kyu Linked Lists

---
+++ codeWars/Linked Lists - Length_&_Count.js
@@ -0,0 +1,26 @@
+/* Linked Lists - Length & Count
+
+Implement Length() to count the number of nodes in a linked list.
+
+length(null) => 0
+length(1 -> 2 -> 3 -> null) => 3
+Implement Count() to count the occurrences of an integer in a linked list.
+
+count(null, 1) => 0
+count(1 -> 2 -> 3 -> null, 1) => 1
+count(1 -> 1 -> 1 -> 2 -> 2 -> 2 -> 2 -> 3 -> 3 -> null, 2) => 4 */
+
+
+function Node(data) {
+  this.data = data
+  this.next = null
+}
+
+function length(head) {
+  return head ? 1 + length(head.next) : 0
+}
+
+function count(head, data) {
+  if (!head) return 0
+  return (head.data === data ? 1 : 0) + count(head.next, data)
+}
