c1e60a023c88b418c347595c1a4eb999cb6ba098
Edgar <edgarsargsyan@hotmail.com>
2023-03-16 12:00:45 -0600

    Solution for the kata WeIrD StRiNg CaSe

---
+++ codeWars/WeIrD_StRiNg_CaSe.js
@@ -0,0 +1,27 @@
+/* Write a function toWeirdCase that accepts a string, 
+and returns the same string with all even indexed characters in each word upper cased, 
+and all odd indexed characters in each word lower cased. The indexing just explained is zero based, 
+so the zero-ith index is even, 
+therefore that character should be upper cased and you need to start over for each word.
+
+The passed in string will only consist of alphabetical characters and spaces(' ').
+Spaces will only be present if there are multiple words. Words will be separated by a single space(' ').
+
+Examples:
+toWeirdCase( "String" );//=> returns "StRiNg"
+toWeirdCase( "Weird string case" );//=> returns "WeIrD StRiNg CaSe" */
+
+function toWeirdCase(string) {
+  let newStr = string.split(" ");
+  return newStr.map((e) => {
+    return [...e].map((e, i) => {
+      return i % 2 === 0 ? e.toUpperCase() : e.toLowerCase()
+    }).join("");
+  }).join(" ");
+
+}
+
+console.log(toWeirdCase('This') === 'ThIs');
+console.log(toWeirdCase('is') === 'Is');
+console.log(toWeirdCase('This is a test') ==='ThIs Is A TeSt');
+console.log(toWeirdCase("Weird string case")=== "WeIrD StRiNg CaSe");
