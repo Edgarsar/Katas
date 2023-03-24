ff05ef28a13bf2d14c5574008e1d3f1375e1e548
Edgar <edgarsargsyan@hotmail.com>
2023-03-24 14:15:53 -0600

    Write a function that will print all items in a nested array using recursion

--- recursion.js
+++ recursion.js
@@ -3,20 +3,35 @@
 function countEvenToTwelve(number) {
   if (number <= 12) {
     console.log(number);
-    countEvenToTwelve(number + 2)
+    countEvenToTwelve(number + 2);
   }
 }
 
-countEvenToTwelve(0);
+// countEvenToTwelve(0);
 
 
 // return the sum of every whole number from n down to 1
 
 function sumtoOne(n) {
-if (n===1){
-  return 1
-}
-  return n + sumtoOne(n-1)
+  if (n === 1) {
+    return 1;
+  }
+  return n + sumtoOne(n - 1);
 }
 
-console.log(sumtoOne(4))
+// console.log(sumtoOne(4));
+
+
+// prints all items in an array
+
+const printItems = function (array) {
+  array.forEach((item) => {
+    if (Array.isArray(item)) {
+      printItems(item);
+    } else {
+      console.log(item);
+    }
+  });
+}
+
+console.log(printItems(["A", ["B", "C"], ["U", ["I", ["O", "Y"]], "L"], "D", "E"]))
