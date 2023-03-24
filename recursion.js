516396d15ce88d00960fc5f8f403174bfd9b9838
Edgar <edgarsargsyan@hotmail.com>
2023-03-24 14:45:42 -0600

    Make sumItems a recursive function that sums all items in an array

--- recursion.js
+++ recursion.js
@@ -46,4 +46,23 @@ function sum(fromN, toN) {
   return toN + sum(fromN, toN - 1)
 }
 
-console.log(sum(3, 7));
+// console.log(sum(3, 7));
+
+
+
+// sums all the numbers in an array.
+function sumItems(array) {
+  return array.reduce((aggr, val) => {
+    if (Array.isArray(val)) {
+      return aggr + sumItems(val)
+    }
+
+    return aggr + val;
+  }, 0)
+
+}
+
+console.log(sumItems([25])) // output 25
+console.log(sumItems([1, 3, 3, 5, 5])) // output 17
+console.log(sumItems([[1, 2, [[3], 4]], 5, []])) // output 15
+console.log(sumItems([[[5], [[[4]]], [[3], 2]], 1, []])) // output 15
