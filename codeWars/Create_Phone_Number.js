a04f07c82d58811e637db097b1ea024ab0b1e332
Edgar <edgarsargsyan@hotmail.com>
2023-04-06 16:46:24 -0600

    Add an alternative solution with the reduce

--- codeWars/Create_Phone_Number.js
+++ codeWars/Create_Phone_Number.js
@@ -14,6 +14,13 @@ const createPhoneNumber = (numbers) => {
 };
 
 
+//Alternative solution
+// const createPhoneNumber = (n) => {
+//   return n.reduce((p, c) => p.replace("x", c), "(xxx) xxx-xxxx");
+// };
+
+
+
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890");
 console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), "(111) 111-1111");
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890");
