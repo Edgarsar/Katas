0857a4ade4897c3c3e163806294a749aa5cefcb6
Edgar <edgarsargsyan@hotmail.com>
2023-05-25 00:06:30 -0600

    Two Joggers

---
+++ codeWars/Two_Joggers.js
@@ -0,0 +1,58 @@
+/* 
+
+Description
+Bob and Charles are meeting for their weekly jogging tour. 
+They both start at the same spot called "Start" and they each run a different lap, 
+which may (or may not) vary in length. Since they know each other for a long time already, 
+they both run at the exact same speed.
+
+Task
+Your job is to complete the function nbrOfLaps(x, y) that, given the length of the laps for Bob and Charles,
+finds the number of laps that each jogger has to complete before they meet each other again,
+at the same time, at the start.
+
+The function takes two arguments:
+
+The length of Bob's lap (larger than 0)
+The length of Charles' lap (larger than 0)
+
+The function should return an array containing exactly two numbers:
+
+The first number is the number of laps that Bob has to run
+The second number is the number of laps that Charles has to run
+
+Examples:
+
+nbrOfLaps(5, 3); // returns [3, 5]
+nbrOfLaps(4, 6); // returns [3, 2] 
+
+*/
+
+const nbrOfLaps = function (x, y) {
+  let xLap = x;
+  let yLap = y;
+
+  for (let i = 0; i <= x; i++) {
+    for (let j = 0; j <= y; j++) {
+
+      if (yLap < xLap) {
+        yLap += y;
+      }
+      if (yLap > xLap) {
+        xLap += x;
+      }
+
+      if (xLap === yLap) {
+
+        xLap = xLap / x
+        yLap = yLap / y
+        return [xLap, yLap]
+      }
+    }
+  }
+};
+
+
+console.log(nbrOfLaps(5, 3), [3, 5])
+console.log(nbrOfLaps(4, 6), [3, 2])
+console.log(nbrOfLaps(5, 5), [1, 1])
