78fa9a23c7dadaab6312af47340fdbd702610b22
Edgar <edgarsargsyan@hotmail.com>
2023-06-01 19:02:41 -0600

    Run length encoding

---
+++ codeWars/Run_length_encoding.js
@@ -0,0 +1,81 @@
+/* 
+
+Run-length encoding (RLE) is a very simple form of data compression 
+in which runs of data (that is, sequences in which the same data value occurs in many
+consecutive data elements) are stored as a single data value and count, rather than as the original run. 
+Wikipedia
+
+Task
+Your task is to write such a run-length encoding.
+For a given string, return a list (or array) of pairs (or arrays)
+[ (i1, s1), (i2, s2), …, (in, sn) ], such that one can reconstruct
+the original string by replicating the character sx ix times and concatening all those strings. 
+Your run-length encoding should be minimal, ie. for all i the values si and si+1 should differ.
+
+Examples
+As the article states, RLE is a very simple form of data compression. It's only suitable for runs of data, 
+as one can see in the following example:
+
+runLengthEncoding("hello world!")
+ =>      [[1,'h'], [1,'e'], [2,'l'], [1,'o'], [1,' '], [1,'w'], [1,'o'], [1,'r'], [1,'l'], [1,'d'], [1,'!']]
+It's very effective if the same data value occurs in many consecutive data elements:
+
+runLengthEncoding("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbb")
+ => [[34,'a'], [3,'b']]
+ 
+ */
+
+
+// const runLengthEncoding = function (str) {
+//   let ans = [];
+
+//   for (let i = 0; i < str.length; i++) {
+//     let count = 1;
+//     while (i < str.length && str[i] == str[i + 1]) {
+//       count++;
+//       i++;
+//     }
+
+
+//     ans.push([count, str[i]]);
+//   }
+//   return ans;
+// }
+
+
+const runLengthEncoding = function (str) {
+  let ans = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let count = 1;
+    while (i < str.length && str[i] == str[i + 1]) {
+      count++;
+      i++;
+    }
+
+
+    ans.push([count, str[i]]);
+  }
+  return ans;
+}
+
+// const runLengthEncoding = function(str){
+//   let arr = []
+//   let count = 1
+//   for (let i = 0; i < str.length ; i++) {
+//     if (str[i] === str[i + 1]) {
+//       count++
+//     } else {
+//       arr.push([count, str[i]])
+//       count = 1
+//     }  
+//   }
+//   return arr 
+// }
+
+
+// console.log(runLengthEncoding(""), []);
+// console.log(runLengthEncoding("abc"), [[1, 'a'], [1, 'b'], [1, 'c']]);
+// console.log(runLengthEncoding("aab"), [[2, 'a'], [1, 'b']]);
+console.log(runLengthEncoding("hello world!"),
+  [[1, 'h'], [1, 'e'], [2, 'l'], [1, 'o'], [1, ' '], [1, 'w'], [1, 'o'], [1, 'r'], [1, 'l'], [1, 'd'], [1, '!']]);
