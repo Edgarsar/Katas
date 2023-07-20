07ec883732ead8689fd1a495f095263995f5cd43
Edgar <edgarsargsyan@hotmail.com>
2023-07-19 20:59:44 -0600

    Length_and_two_values

--- codeWars/Length_and_two_values.js
+++ codeWars/Length_and_two_values.js
@@ -29,8 +29,8 @@ function alternate(n, firstValue, secondValue) {
 
 
 
-console.log(alternate(5, true, false), [true, false, true, false, true])
+console.log(alternate(5, true, false), [true, false, true, false, true]);
 
-console.log(alternate(20, 'blue', 'red'), ['blue', 'red', 'blue', 'red', 'blue', 'red', 'blue', 'red', 'blue', 'red', 'blue', 'red', 'blue', 'red', 'blue', 'red', 'blue', 'red', 'blue', 'red'])
+console.log(alternate(20, 'blue', 'red'), ['blue', 'red', 'blue', 'red', 'blue', 'red', 'blue', 'red', 'blue', 'red', 'blue', 'red', 'blue', 'red', 'blue', 'red', 'blue', 'red', 'blue', 'red']);
 
-console.log(alternate(0, "lemons", "apples"), [])
+console.log(alternate(0, "lemons", "apples"), []);
